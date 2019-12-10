5c15817850888138f51f364794dd52f302afc730
Dmitrii Tcimbal <dtcimbal@gmail.com>
2019-12-10 18:22:32 +1100

    Adds a builder to create sqsEventHandler

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,5 +1,25 @@
 import 'source-map-support/register';
-import {withSQSEventHandler} from "./sqsEventHandler";
+
+import {Body} from "./Body";
+import {SQSEvent} from "aws-lambda";
+
+export function withSQSEventHandler(bodyHandler: (body: Body) => void) {
+  return async (event: SQSEvent) => {
+    console.log(`Handled SQS Event ${JSON.stringify(event)}`);
+
+    // Processing messages in async manier
+    await Promise.all(event.Records.map(sqsRecord => {
+      try {
+        console.info(`parsing sqs message ${JSON.stringify(sqsRecord)}`);
+        const body: Body = JSON.parse(sqsRecord.body);
+        bodyHandler(body);
+
+      } catch (e) {
+        console.error(`Fail to process parsed msg ${sqsRecord.messageId}`, e);
+      }
+    }));
+  };
+}
 
 export const echoSQSEventHandler = withSQSEventHandler((body) => {
   console.log(`processing message body ${JSON.stringify(body)}`);
